e027390c3fef66a0e60e27eda6ca39b12dcdc38e
daniil.gozhenko <daniil.gozhenko@gmail.com>
2021-06-03 04:37:26 +0300

    Manual email change for test purpose

--- app/src/main/java/com/example/expensare/ui/manage_group/members/GroupMembersFragment.kt
+++ app/src/main/java/com/example/expensare/ui/manage_group/members/GroupMembersFragment.kt
@@ -4,21 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.expensare.R
 import com.example.expensare.databinding.FragmentGroupMembersBinding
 import com.example.expensare.ui.base.BaseFragment
-import com.google.firebase.auth.FirebaseAuth
 
-class GroupMembersFragment: BaseFragment() {
+class GroupMembersFragment : BaseFragment() {
     private var _binding: FragmentGroupMembersBinding? = null
-    private val binding get() = _binding!!
+    private val binding
+        get() = _binding!!
 
-    private val groupMembersViewModel: GroupMembersViewModel by lazy { ViewModelProvider(this).get(GroupMembersViewModel::class.java) }
+    private val groupMembersViewModel: GroupMembersViewModel by lazy {
+        ViewModelProvider(this).get(GroupMembersViewModel::class.java)
+    }
 
     override fun inflateView(inflater: LayoutInflater, container: ViewGroup?): View {
         _binding = FragmentGroupMembersBinding.inflate(inflater)
@@ -35,28 +35,29 @@ class GroupMembersFragment: BaseFragment() {
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.add_member_menu_item -> {
-                    groupMembersViewModel.getUserByEmail("zeratskilsotov@gmail.com")
-                    groupMembersViewModel.userByEmail.observe(viewLifecycleOwner, { user->
-                    groupMembersViewModel.addUserToGroup(user)
-                    })
+                    groupMembersViewModel.getUserByEmail("hearthstone.gozhenko@gmail.com")
+                    groupMembersViewModel.userByEmail.observe(
+                        viewLifecycleOwner,
+                        { user -> groupMembersViewModel.addUserToGroup(user) }
+                    )
                     true
-                } else -> false
+                }
+                else -> false
             }
         }
     }
 
     private fun getMembers() {
-        groupMembersViewModel.group.observe(viewLifecycleOwner, {
-            groupMembersViewModel.getUsersFromGroup(it)
-        })
+        groupMembersViewModel.group.observe(
+            viewLifecycleOwner,
+            { groupMembersViewModel.getUsersFromGroup(it) }
+        )
     }
 
     private fun bindRecyclerView() {
         val adapter = GroupMembersAdapter()
         binding.groupMembersRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.groupMembersRecyclerView.adapter = adapter
-        groupMembersViewModel.user.observe(viewLifecycleOwner, {
-            adapter.setUser(it)
-        })
+        groupMembersViewModel.user.observe(viewLifecycleOwner, { adapter.setUser(it) })
     }
 }
